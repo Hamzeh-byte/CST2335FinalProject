2b4e51052a2c74217deae1044a4255336695179e
krisc <char0799@algonquinlive.com>
2020-12-08 11:07:18 -0500

    Updated MainActivity with changes made by Hamzeh

--- app/src/main/java/com/vogella/android/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/vogella/android/cst2335finalproject/MainActivity.java
@@ -4,21 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button covidButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        covidButton = findViewById(R.id.covidButton);
-        Intent goToCovid = new Intent(this, CovidMain.class);
 
-        covidButton.setOnClickListener(click -> startActivity(goToCovid));
+        ImageButton btn_covid19 = (ImageButton)findViewById(R.id.btn_covid19);
+        btn_covid19.setOnClickListener(bt -> {
+           Intent nextPage = new Intent(MainActivity.this, CovidMain.class);
+            startActivity(nextPage);
+        });
 
+        ImageButton btn_recipe = (ImageButton)findViewById(R.id.btn_recipe);
+        btn_recipe.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
+
+
+        ImageButton btn_ticketevent = (ImageButton)findViewById(R.id.btn_ticketevent);
+        btn_ticketevent.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
+
+        ImageButton btn_audiodb = (ImageButton)findViewById(R.id.btn_audiodb);
+        btn_audiodb.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
     }
 }
