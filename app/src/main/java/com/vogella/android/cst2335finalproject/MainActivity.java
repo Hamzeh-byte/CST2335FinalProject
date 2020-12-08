ebb9fa50e8d7b8b74efdba4a8ee80b32e1bf3239
Hamzeh-byte <kadd0024@algonquinlive.com>
2020-12-08 07:48:09 -0500

    Added a Layout and Main Page with corresponding button clickListeners.

--- app/src/main/java/com/vogella/android/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/vogella/android/cst2335finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.vogella.android.cst2335finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton btn_covid19 = (ImageButton)findViewById(R.id.btn_covid19);
+        btn_covid19.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
+
+        ImageButton btn_recipe = (ImageButton)findViewById(R.id.btn_recipe);
+        btn_recipe.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
+
+
+        ImageButton btn_ticketevent = (ImageButton)findViewById(R.id.btn_ticketevent);
+        btn_ticketevent.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
+
+        ImageButton btn_audiodb = (ImageButton)findViewById(R.id.btn_audiodb);
+        btn_audiodb.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, .class);
+//            startActivity(nextPage);
+        });
     }
 }
